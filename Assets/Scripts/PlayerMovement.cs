950d0ab51bff83cab8b1132ad35ecac0ee150f20
Arnav Singh <arnav010singh@gmail.com>
2024-03-04 10:34:45 +0530

    fc

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Transform orientation;
+    public Transform gunOrientation;
+
+
+    private Rigidbody rb;
+    public float movementSpeed;
+
+    public Transform instancePosition;
+
+    public float bulletSpeed;
+    public GameObject bulletPrefab;
+
+    private bool shouldShoot = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        StartCoroutine(ShootInterval());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            shouldShoot = true;
+        }
+        else
+        {
+            shouldShoot = false;
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Vector3 direction = orientation.forward * Input.GetAxis("Vertical") + orientation.right * Input.GetAxis("Horizontal");
+        rb.AddForce(direction.normalized * movementSpeed, ForceMode.Force);
+    }
+
+    void ShootBullets()
+    {
+        Debug.Log("Shooting bullet");
+        var instance = Instantiate(bulletPrefab, instancePosition.position, bulletPrefab.transform.rotation);
+        instance.GetComponent<Rigidbody>().AddForce(gunOrientation.forward * bulletSpeed);
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse clicked");
+        ShootBullets();
+    }
+
+    IEnumerator ShootInterval()
+    {
+
+        while (true)
+        {
+            // Your code to be executed every 3 seconds
+            Debug.Log("Executing every 1 seconds!");
+
+            if (shouldShoot)
+            {
+                ShootBullets();
+            }
+
+            // Wait for 3 seconds before the next iteration
+            yield return new WaitForSeconds(0.1f);
+        }
+       
+
+    }
+}
---
+++ Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+
+    public float senX;
+    public float senY;
+
+    public float rotationX; // Vertical Rotation
+    public float rotationY; //Horizontal Rotation
+
+    public Transform orientation;
+
+    public Transform gunOrientation;
+
+    public Transform playerCameraPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * senX * Time.deltaTime;
+        rotationY += mouseX;
+
+
+        float mouseY = Input.GetAxisRaw("Mouse Y") * senX * Time.deltaTime;
+        rotationX -= mouseY;
+
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
+
+        gunOrientation.rotation = Quaternion.Euler(rotationX, rotationY, 0);
+
+        orientation.rotation = Quaternion.Euler(0, rotationY, 0);
+
+        transform.position = playerCameraPos.position;
+
+    }
+}
